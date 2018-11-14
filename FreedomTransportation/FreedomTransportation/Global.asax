f9a04bd80f92ff1a31d5cb8ae683873b47e9f6af
manzilat <amtulnoorq@yahoo.com>
2018-11-14 08:06:01 -0600

    added 3 models and changed the theme

---
+++ FreedomTransportation/FreedomTransportation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FreedomTransportation.MvcApplication" Language="C#" %>
---
+++ FreedomTransportation/FreedomTransportation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FreedomTransportation.Startup))]
+namespace FreedomTransportation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
