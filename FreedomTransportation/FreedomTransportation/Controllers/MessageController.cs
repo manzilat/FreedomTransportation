5add1a98a497c662d309a9b44af3c30a9bda47ac
manzilat <amtulnoorq@yahoo.com>
2018-11-28 11:57:22 -0600

    added all the files

---
+++ FreedomTransportation/FreedomTransportation/Controllers/MessageController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FreedomTransportation.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Message
+        // GET: /<controller>/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+//        public ActionResult GetMessages()
+//        {
+//            var allMessages = Message.GetMessages();
+//            return View(allMessages);
+//        }
+
+//        public ActionResult SendMessage()
+//        {
+//            return View();
+//        }
+
+//        [HttpPost]
+//        public ActionResult SendMessage(Message newMessage)
+//        {
+//            newMessage.Send();
+//            return RedirectToAction("Index");
+//        }
+//    }
+//}
